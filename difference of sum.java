16d08a3f3447a17d2918cc12e69ef673a8d655bb
Smartpraga <gayathripriya164@gmail.com>
2026-06-29 20:31:18 +0530

    difference of sum.java

---
+++ difference of sum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int differenceOfSum(int[] nums) {
+        int elementSum = 0;
+        int digitSum = 0;
+
+        for (int num : nums) {
+            elementSum += num;
+            
+            // Calculate sum of digits for the current number
+            while (num > 0) {
+                digitSum += num % 10;
+                num /= 10;
+            }
+        }
+
+        return Math.abs(elementSum - digitSum);
+    }
+}
